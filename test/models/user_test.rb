5f4f1f8cbf4dbdc4dc69a52cdcbcd7a1193c5dd4
ottagit <ottachris13@yahoo.com>
2016-12-05 17:41:58 +0300

    validate email format with regex
    Make sure that the regular expression
    disallows double quotes in email domain names

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -6,7 +6,7 @@ class UserTest < ActiveSupport::TestCase
   # end
 
   def setup
-   @user = User.new(name: "Example User", email: "user.example.com")
+   @user = User.new(name: "Example User", email: "user@example.com")
   end
 
   test 'should be valid' do
@@ -41,6 +41,15 @@ class UserTest < ActiveSupport::TestCase
       # include optional 2nd arg to the assertion with a custom error msg
       # which identifies the specific address causing the test to fail and not just the line number
       assert @user.valid?, "#{valid_address.inspect} should be valid"
+    end
+  end
+
+  test 'email validation should reject invalid email addresses' do
+    invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
+                           foo@bar_baz.com foo@bar+baz.com]
+    invalid_addresses.each do |invalid_address|
+      @user.email = invalid_address
+      assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
     end
   end
 end
