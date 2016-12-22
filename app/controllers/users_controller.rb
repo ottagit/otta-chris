b5c9de40c3502d8b110cdecdd451047cab5c7d96
ottagit <ottachris13@yahoo.com>
2016-12-22 13:15:35 +0300

    use strong parameters in the create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,11 +10,17 @@ class UsersController < ApplicationController
   end
 
   def create
-   @user = User.new(params[:user]) # Not the final implementation
+   @user = User.new(params[user_params]) # Not the final implementation
    if @user.save
      # Handle a successful save
    else
     render 'new'
    end
   end
+
+  private
+  # Auxiliary method to be used internally by the Users controller
+   def user_params
+     params.require(:user).permit(:name, :email, :password, :password_confirmation)
+   end
 end
