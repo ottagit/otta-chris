029b162acc9d48be756759dbbf8d7584d2455383
ottagit <ottacc66@gmail.com>
2017-01-21 15:39:35 +0300

    define remember helper to remember a user

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,16 +1,26 @@
 module SessionsHelper
  
- #Logs in the given user
+ # Logs in the given user
  def log_in(user)
   session[:user_id] = user.id
  end
 
- #Returns the current logged-in user(if any)
+ # Remembers a user in a persistent session
+ def remember(user)
+  # Generate a remember token and create save its digest to the db
+  user.remember
+  # Use cookies to create permanent cookies for the user id 
+  # and remember token
+  cookies.permanent.signed[:user_id] = user.id 
+  cookies.permanent[:remember_token] = user.remember_token
+ end
+
+ # Returns the current logged-in user(if any)
  def current_user
   @current_user ||= User.find_by(id: session[:user_id])
  end
 
- #Returns true if the user is logged in, false otherwise
+ # Returns true if the user is logged in, false otherwise
  def logged_in?
   !current_user.nil?
  end
