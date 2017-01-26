093225d787ec093c862089583966938108407f09
ottagit <ottacc66@gmail.com>
2017-01-26 16:34:06 +0300

    eyeballs on script

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,8 +4,8 @@ class SessionsController < ApplicationController
 
   def create
    @user = User.find_by(email: params[:session][:email].downcase)
-   if @user && user.authenticate(params[:session][:password]) 
-    log_in user
+   if @user && @user.authenticate(params[:session][:password]) 
+    log_in @user
     params[:session][:remember_me] == '1' ? remember(@user) : forget(@user) 
     redirect_to @user
    else
