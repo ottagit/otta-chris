c1745e0077ff2ac82c0004a94768851006fa46d2
ottagit <ottacc66@gmail.com>
2017-02-02 13:22:56 +0300

    log in a test user before edit

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -10,6 +10,7 @@ class UsersEditTest < ActionDispatch::IntegrationTest
   end
 
   test "unsuccessful edit" do
+    log_in_as(@user)
     get edit_user_path(@user)
     assert_template 'users/edit'
     patch user_path(@user), params: { user: { name: "",
@@ -21,6 +22,7 @@ class UsersEditTest < ActionDispatch::IntegrationTest
   end
 
   test "successful edit" do
+    log_in_as(@user)
     get edit_user_path(@user)
     assert_template 'users/edit'
     name = "Chris Otta"
@@ -30,7 +32,7 @@ class UsersEditTest < ActionDispatch::IntegrationTest
                                               password:              "",
                                               password_confirmation: ""} }
     assert_not flash.empty?
-    assert_redirect_to @user
+    assert_redirected_to @user
     @user.reload # reloads user's values from db, confirming a successful update
     assert_equal name, @user.name
     assert_equal email, @user.email
