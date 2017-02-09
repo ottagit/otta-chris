b1a7e3c139235069157972865ce6a65558567769
ottagit <ottacc66@gmail.com>
2017-02-09 11:09:27 +0300

    test for all the layout links
    This includes the proper behaviour for
    logged-in and non-logged in users.

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -4,15 +4,30 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+  
+  def setup
+   @user = users(:chris)
+  end
 
   test "layout links" do
     get root_path
     assert_template 'static_pages/home'
     assert_select  "a[href=?]", root_path, count: 2
     assert_select  "a[href=?]", help_path
+    assert_select  "a[href=?]", login_path
     assert_select  "a[href=?]", about_path
     assert_select  "a[href=?]", contact_path
     get signup_path
     assert_select  "title", full_title("Sign up")
+    log_in_as(@user)
+    assert is_logged_in?
+    assert_redirected_to @user
+    follow_redirect!
+    assert_template 'users/show'
+    assert_select  "a[href=?]", users_path    
+    assert_select  "a[href=?]", login_path, count: 0
+    assert_select  "a[href=?]", user_path
+    assert_select  "a[href=?]", edit_user_path
+    assert_select  "a[href=?]", logout_path 
   end
 end
