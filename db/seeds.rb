1499420e21855713c48d2d136a1f1a8ae627a1dc
ottagit <ottacc66@gmail.com>
2017-02-09 14:19:45 +0300

    change user email to gmail

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create!(name:  "Sisi Hub",
-             email: "ottachris13@yahoo.com",
+User.create!(name:  "Chris Otta",
+             email: "ottacc66@gmail.com",
              password:                  "password",
              password_confirmation:     "password")
 
