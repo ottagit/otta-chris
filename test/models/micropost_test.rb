0a86318cf228a3ab224daf21ad9f944c20e8caea
ottagit <ottacc66@gmail.com>
2017-02-24 13:52:04 +0300

    test for the validity of a new micropost

--- test/models/micropost_test.rb
+++ test/models/micropost_test.rb
@@ -4,4 +4,19 @@ class MicropostTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  def setup
+   @user = users(:chris)
+   # This code is not idiomatically correct and therefore makes test to pass without validations( > rails 5)
+   @micropost = Micropost.new(content: "Lorem ipsum", user_id: @user.id)
+  end
+ 
+  test "should be valid" do
+    assert @micropost.valid?
+  end
+
+  test "user id should be present" do
+    @micropost.user_id = nil
+    assert_not @micropost.valid?
+  end
 end
