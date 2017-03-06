6610ab5b7d79165b277eec1fec09cf4f5950d7a3
ottagit <ottacc66@gmail.com>
2017-03-06 15:49:24 +0300

    move the logged_in_user method into the app ctrlr

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -51,21 +51,13 @@ class UsersController < ApplicationController
   
   private
   # Auxiliary method to be used internally by the Users controller
+
    def user_params
      params.require(:user).permit(:name, :email, :password, :password_confirmation)
    end
 
   # Before filters
 
-  # Confirms a logged-in user
-   def logged_in_user
-    unless logged_in?
-      store_location
-      flash[:danger] = "Please log in."
-      redirect_to login_url
-    end
-   end
-  
   # Confirms the correct user
    def correct_user
     @user = User.find(params[:id])
