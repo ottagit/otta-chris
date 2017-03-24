503bc951948e51d6592368e38f2f4c6e2aa8bf92
ottagit <ottacc66@gmail.com>
2017-03-24 13:20:41 +0300

    test the Relationship model validations

--- test/models/relationship_test.rb
+++ test/models/relationship_test.rb
@@ -4,4 +4,22 @@ class RelationshipTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  
+  def setup
+    @relationship = Relationship.new(follower_id: users(:chris).id, followed_id: users(:ben).id)
+  end
+
+  test "should be valid" do
+    assert @relationship.valid?
+  end
+
+  test "should require a follwer_id" do
+    @relationship.follower_id = nil
+    assert_not @relationship.valid?
+  end
+
+  test "should require a followed_id" do
+    @relationship.followed_id = nil
+    assert_not @relationship.valid?
+  end
 end
