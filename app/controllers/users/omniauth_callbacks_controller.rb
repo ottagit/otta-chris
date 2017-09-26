1465b69f05766cebce6166d71f01c9e6dc6c771f
ottagit <ottacc66@gmail.com>
2017-09-26 12:25:36 +0300

    reove typo

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,7 +1,7 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   def twitter 
-    @user = User.from_omniauth(request.env["omniauth.auth"]#.except("extra")
+    @user = User.from_omniauth(request.env["omniauth.auth"])
 
     if @user.persisted?
       sign_in_and_redirect @user, :event => :authentication # this will throw if @user is not activated
